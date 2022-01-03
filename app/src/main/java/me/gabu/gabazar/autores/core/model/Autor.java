41268dd764cf82123eb8de84a651a24ad3ecaf18
Gabriel Moreira <joaogmrodrigues@outlook.com>
2022-01-03 19:52:28 -0300

    ⏪️ Revert POM

--- app/src/main/java/me/gabu/gabazar/autores/core/model/Autor.java
+++ app/src/main/java/me/gabu/gabazar/autores/core/model/Autor.java
@@ -16,10 +16,16 @@ import me.gabu.gabazar.autores.service.validations.Update;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Autor {
-    private @NotNull(groups = Update.class) String id;
-    private @NotNull(groups = { Update.class, Create.class }) String nome;
-    private @NotNull(groups = { Update.class, Create.class }) String nacionalidade;
-    private @NotNull(groups = { Update.class, Create.class }) String site;
+
+    private static final String MSG_NAC_NULO = "[nacionalidade não deve ser nulo]";
+    private static final String MSG_SITE_NULO = "[site não deve ser nulo]";
+    private static final String MSG_NOME_NULO = "[nome não deve ser nulo]";
+    private static final String MSG_ID_NULO = "[id não deve ser nulo]";
+
+    private @NotNull(groups = Update.class, message = MSG_ID_NULO) String id;
+    private @NotNull(groups = { Update.class, Create.class }, message = MSG_NOME_NULO) String nome;
+    private @NotNull(groups = { Update.class, Create.class }, message = MSG_NAC_NULO) String nacionalidade;
+    private @NotNull(groups = { Update.class, Create.class }, message = MSG_SITE_NULO) String site;
     private Date dataCriacao;
     private Date dataAlteracao;
     private String usuarioCriacao;
